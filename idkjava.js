6f2366576a60233cbb0437ee3d5b62d918fad7d1
Jasmine Wallin <jwallin2@uoregon.edu>
2024-03-04 22:23:16 -0800

    great updates

--- idkjava.js
+++
@@ -1,5 +0,0 @@
-let	l = document.getElementById('myUL').getElementsByTagName('li');
-
-// list items:
-for(let i = 0; i<l.length; i++)
-	document.getElementById('list').innerHTML += l.item(i).innerHTML;
